00aadab57184b7573c78a70785700ad52c71dcfb
faisal-t <faizalmahromi35@gmail.com>
2021-09-27 01:58:31 +0700

    heroku

--- config/config.go
+++ config/config.go
@@ -7,27 +7,27 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// const (
-// 	username string = "sql6439664"
-// 	password string = "Pae356TDXy"
-// 	host     string = "sql6.freesqldatabase.com"
-// 	port     int    = 3306
-// 	database string = "sql6439664"
-// )
-
-// var (
-// 	dsn = fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
-// )
-
 const (
-	username string = "root"
-	password string = ""
-	database string = "final-project-golang"
+	username string = "sql6439664"
+	password string = "Pae356TDXy"
+	host     string = "sql6.freesqldatabase.com"
+	port     int    = 3306
+	database string = "sql6439664"
 )
 
 var (
-	dsn = fmt.Sprintf("%v:%v@/%v", username, password, database)
+	dsn = fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 )
+
+// const (
+// 	username string = "root"
+// 	password string = ""
+// 	database string = "final-project-golang"
+// )
+
+// var (
+// 	dsn = fmt.Sprintf("%v:%v@/%v", username, password, database)
+// )
 
 // HubToMySQL
 func MySQL() (*sql.DB, error) {
